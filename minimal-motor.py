2db85df8fe77dd5f05ffd939dcbf7f6043a5ec7c
mantypet <petteri.mantymaa@helsinki.fi>
2018-10-19 14:18:57 +0300

    Minimal viable motor & text-to-speech

---
+++ minimal-motor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import ev3dev.ev3 as ev3
+import time
+
+m = ev3.LargeMotor('outA')
+m.run_timed(time_sp=3000, speed_sp=500)
+
+time.sleep(2)
+ev3.Sound.speak('Hello world').wait()
