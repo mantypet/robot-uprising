93e0242df7be4352ca9981c4eab8caf7ce8282b8
Rsl1122 <djrapitops@hotmail.com>
2018-10-19 14:36:53 +0300

    [New] Led color enumeration utility

---
+++ utils/ledcolor.py
@@ -0,0 +1,11 @@
+# 'RED', 'GREEN', 'YELLOW', 'ORANGE, 'AMBER', 'BLACK'
+class LedColor:
+    RED = "RED"
+    GREEN = "GREEN"
+    YELLOW = "YELLOW"
+    ORANGE = "ORANGE"
+    AMBER = "AMBER"
+    BLACK = "BLACK"
+
+    RIGHT = "RIGHT"
+    LEFT = "LEFT"
