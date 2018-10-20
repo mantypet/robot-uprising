e2e08179ba26ff853a95f395f53a24af56e4dbaf
Rsl1122 <djrapitops@hotmail.com>
2018-10-20 15:14:27 +0300

    Fix Range finder

--- tank/custom_tank.py
+++ tank/custom_tank.py
@@ -1,7 +1,7 @@
 from ev3dev2.motor import MoveTank
 
-class CustomMoveTank(MoveTank):
 
+class CustomMoveTank(MoveTank):
 
     def turn(self, degrees):
 
@@ -17,20 +17,25 @@ class CustomMoveTank(MoveTank):
         degrees = degrees - rotations * 360
 
         # Set all parameters
-        self.left_motor.on_for_rotations(speed, rotations, block=False)
-        self.right_motor.on_for_rotations(-speed, rotations, block=True)
-        self.left_motor.on_for_degrees(speed, degrees=degrees, block=False)
-        self.right_motor.on_for_degrees(-speed, degrees=degrees, block=False)
+        if rotations != 0:
+            self.left_motor.on_for_rotations(speed, rotations, block=False)
+            self.right_motor.on_for_rotations(-speed, rotations, block=True)
+        if degrees != 0:
+            self.left_motor.on_for_degrees(speed, degrees=degrees, block=False)
+            self.right_motor.on_for_degrees(-speed,
+                                            degrees=degrees, block=False)
 
     def move_cm(self, centimeters):
         degrees = centimeters * 108
 
         rotations = (degrees - degrees % 360) / 360
         degrees = degrees - rotations * 360
-        speed = -60
+        speed = 60 if degrees > 0 else -60
 
         # Set all parameters
-        self.left_motor.on_for_rotations(speed, rotations, block=False)
-        self.right_motor.on_for_rotations(speed, rotations, block=True)
-        self.left_motor.on_for_degrees(speed, degrees=degrees, block=False)
-        self.right_motor.on_for_degrees(speed, degrees=degrees, block=False)
+        if rotations != 0:
+            self.left_motor.on_for_rotations(speed, rotations, block=False)
+            self.right_motor.on_for_rotations(speed, rotations, block=True)
+        if degrees != 0:
+            self.left_motor.on_for_degrees(speed, degrees=degrees, block=False)
+            self.right_motor.on_for_degrees(speed, degrees=degrees, block=False)
