0b74f8f3b6f825979561ea4cf62ef0663362c969
Rsl1122 <djrapitops@hotmail.com>
2018-10-20 23:07:03 +0300

    Fixed compile errors in wline

--- wline/wline.py
+++ wline/wline.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from time import sleep
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank, MoveSteering
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM
 # degrees per second, rotations per minute, rotations per second, degrees per minute
@@ -9,23 +10,20 @@ from ev3dev2.display import Display
 from ev3dev2.button import Button
 from ev3dev2.sound import Sound
 
-#valkoinen mitattu 70, harmaa mitattu 12, keskiarvo ^40
+# valkoinen mitattu 70, harmaa mitattu 12, keskiarvo ^40
 
-#taustavari
+# taustavari
 ridColor = 40
 
-from time import sleep
 
 button = Button()
-colorS = ColorSensor(INPUT_4)
+colorS = ColorSensor(INPUT_3)
 sound = Sound()
 
-#ajaa eteenpain
+# ajaa eteenpain
 tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
-#kaantyy 2 sekunttia 
-tank_rotate = tank_pair.on_for_seconds(OUTPUT_B, OUTPUT_C, 2, brake=True, block=False)
+# kaantyy 2 sekunttia
 
-tank_rotate.off()
 tank_pair.off()
 
 sound.beep()
@@ -33,10 +31,9 @@ sound.beep()
 while True:
     intensity = colorS.reflected_light_intensity
 
-    if (intensity > ridColor): # viivalla
-        tank_pair.on(-50, -50)  # vasemman ja oikean nopeudet, ajaa eteenpäin
+    if (intensity > ridColor):  # viivalla
+        tank_pair.on(50, 50)  # vasemman ja oikean nopeudet, ajaa eteenpäin
 
-    else:        
-        tank_rotate.on(-25, 25)  # vasemman ja oikean nopeudet, kääntyy vasemmalle
-        tank_rotate.on(25, -25)  # vasemman ja oikean nopeudet, kääntyy oikealle
-
+    else:
+        tank_pair.on_for_seconds(-25, 25, 2, brake=True, block=True) # vasemman ja oikean nopeudet, kääntyy vasemmalle
+        tank_pair.on_for_seconds(25, -25, 2, brake = True, block = True)  # vasemman ja oikean nopeudet, kääntyy oikealle
