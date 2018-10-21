612cdcb273432deb822c3ae62e78ba3b46a11ff4
Rsl1122 <djrapitops@hotmail.com>
2018-10-21 11:34:06 +0300

    Line following algorithm to PS3 control

--- ps3control.py
+++ ps3control.py
@@ -8,6 +8,12 @@ import evdev
 
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MoveSteering
 from ev3dev2.sound import Sound
+from time import sleep
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor
+from ev3dev2.led import Leds
+from ev3dev2.display import Display
+from ev3dev2.button import Button
 
 import threading
 import math
@@ -52,6 +58,8 @@ close_jaw = True
 closed = False
 running = True
 
+follow_line = False
+
 
 class MotorThread(threading.Thread):
     def __init__(self):
@@ -60,6 +68,8 @@ class MotorThread(threading.Thread):
 
     def run(self):
         while running:
+            if follow_line:
+                continue
             if speedL != 0 or speedR != 0:
                 self.tank.on(-speedL, -speedR)
             else:
@@ -89,12 +99,66 @@ class ClawThread(threading.Thread):
         self.claw.off()
 
 
+class LineFollowThread(threading.Thread):
+    def __init__(self):
+        self.claw = LargeMotor(OUTPUT_D)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # valkoinen mitattu 70, harmaa mitattu 12, keskiarvo ^40
+
+        # taustavari
+        # ridColor = 20 # maalarinteippi
+        ridColor = 40  # valkoinen paperi
+
+        # Nopeus
+        speed = 25
+        counter_max = 5
+
+        button = Button()
+        colorS = ColorSensor(INPUT_3)
+        sound = Sound()
+
+        # ajaa eteenpain
+        tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
+        # kaantyy 2 sekunttia
+
+        tank_pair.off()
+
+        sound.beep()
+
+        # Suoritussilmukka
+        while True:
+            intensity = colorS.reflected_light_intensity
+
+            while (intensity > ridColor and follow_line):  # viivalla
+                intensity = colorS.reflected_light_intensity
+                tank_pair.on(50, 50)  # Eteenpäin
+                counter_max = 5  # alustetaan viivanhaun sektorin leveys
+
+            if (intensity <= ridColor and follow_line):  # Ei viivalla -> alusta viivanhakumuuttujat
+                speed = -speed
+
+            i = 0
+            while (intensity <= ridColor and i < counter_max and follow_line):  # Ei viivalla ->
+                intensity = colorS.reflected_light_intensity
+                # vasemman ja oikean nopeudet, kääntyy vasemmalle
+                tank_pair.on(speed, -speed)
+                i += 1
+
+            if follow_line:
+                counter_max += counter_max
+
+
 motor_thread = MotorThread()
 motor_thread.setDaemon(True)
 motor_thread.start()
 claw_thread = ClawThread()
 claw_thread.setDaemon(True)
 claw_thread.start()
+line_thread = LineFollowThread()
+line_thread.setDaemon(True)
+line_thread.start()
 
 duck = MediumMotor(OUTPUT_A)
 duck.on(20)
@@ -128,3 +192,7 @@ for event in gamepad.read_loop():  # this loops infinitely
         close_jaw = True
     if event.type == 1 and event.code == 305 and event.value == 1:
         close_jaw = False
+    if event.type == 1 and event.code == 307 and event.value == 1:
+        follow_line = True
+    if event.type == 1 and event.code == 308 and event.value == 1:
+        follow_line = False
